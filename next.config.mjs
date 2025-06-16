1d5e17f0e4f0f2a3a2e3b3563366f213d89c3d69
treystr <67973734+treystr@users.noreply.github.com>
2025-06-16 18:07:22 -0400

    deployment updates for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/bitcoin-center' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/bitcoin-center/' : '',
+};
 
 export default nextConfig;
